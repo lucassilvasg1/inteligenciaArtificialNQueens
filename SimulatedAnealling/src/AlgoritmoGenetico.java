3fa4b87fecd2c7bf76119cdd71cbf8b749946154
Marcos Lunciel <marcoslunciel@hotmail.com>
2019-10-31 23:57:17 -0300

    funcao de mutacao

--- SimulatedAnealling/src/AlgoritmoGenetico.java
+++ SimulatedAnealling/src/AlgoritmoGenetico.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -85,5 +87,32 @@ public class AlgoritmoGenetico
       
       return f1;
    }
+   
+   public static void mutacao(List<Integer> jogo)
+   {
+	   int dimensao = jogo.size();
+	   
+	   List<Integer> auxiliar = new ArrayList<Integer>();
+	   for (int i = 0; i < dimensao; i++) {
+		   auxiliar.add(i);
+	   }
+	   
+	   List<Integer> pecas = AlgoritmoGenetico.pickNRandomElements(auxiliar, 2, ThreadLocalRandom.current());
+	   
+	   jogo.set(pecas.get(1), pecas.get(0));
+   }
+   
+   public static <E> List<E> pickNRandomElements(List<E> list, int n, Random r) {
+	    int length = list.size();
+
+	    if (length < n) return null;
+
+	    //We don't need to shuffle the whole list
+	    for (int i = length - 1; i >= length - n; --i)
+	    {
+	        Collections.swap(list, i , r.nextInt(i + 1));
+	    }
+	    return list.subList(length - n, length);
+}
    
 }
